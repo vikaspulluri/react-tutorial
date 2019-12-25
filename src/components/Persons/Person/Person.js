8ad5974e24346526a21aa52ae5385d43a4c1f180
wse_vikaspulluri <vikas.pulluri@wallstreetenglish.com>
2019-12-25 16:47:31 +0530

    Refs

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Person.css';
-import PropTypes form 'prop-types';
+import PropTypes from 'prop-types';
 
 class Person extends React.Component {
     style = {
@@ -8,13 +8,27 @@ class Person extends React.Component {
             width: '450px'
         }
     };
+    constructor(props) {
+      super(props);
+      this.inputElementRef = React.createRef();
+    }
+    componentDidMount() {
+      // this.inputElement.focus();
+      this.inputElementRef.current.focus();
+    }
     render() {
       console.log('[Person.js] rendering...');
       return (
           <div className="Person" style={this.style}>
               <p onClick={this.props.click}>I am {this.props.name} and my age is {this.props.age}</p>
               <p>{this.props.children}</p>
-              <input onChange={this.props.changed} value={this.props.name}></input>
+              <input
+                onChange={this.props.changed}
+                value={this.props.name}
+                // ref={(inputEle) => {this.inputElement = inputEle}}
+                ref={this.inputElementRef}
+                >
+              </input>
           </div>
       );
     }
